758dc8528409982275817b25ccb753ca46e3b703
Rakavy Ravisingam <rakavy_12@hotmail.com>
2016-11-18 18:56:20 -0500

    find frequency of words, create dictionary of words, index the tweets with the dictionnary

--- LSTM/LSTMSentiment.py
+++ LSTM/LSTMSentiment.py
@@ -1,8 +1,9 @@
 import pandas as pnd
 import numpy as np
 import nltk
-nltk.download('punkt')
+#nltk.download('punkt')
 
+popularWords = 5000
 
 def processString(stro):
     return stro.translate(None, '@#')
@@ -14,7 +15,6 @@ def readCSV(filePath):
     csvTwitter=pnd.read_csv(filePath,header=None).values
 
     tSentiments=csvTwitter[:,0]
-
     textData=preprocess(csvTwitter[:,5])
 
     return (tSentiments, textData)
@@ -22,18 +22,62 @@ def readCSV(filePath):
 
 def tokenzieSentence(tweets):
     tokenizedPhrase = []
+   
     for phrase in tweets:
         tokenizedPhrase.append([word[0] for word in nltk.pos_tag(nltk.word_tokenize(phrase)) if word[1] not in ['CC','IN']])
-
-
-
 
     return tokenizedPhrase
 
 
+def wordFrequency(phraseArray):
+    wordfreq = dict()
+
+    for phrase in phraseArray:
+        for word in phrase:
+            if word not in wordfreq:
+                wordfreq[word] = 1
+            else:
+                wordfreq[word] += 1
+
+    return wordfreq
+
+def indexWords(dictWords):
+    counts = dictWords.values()
+    keys = dictWords.keys()
+
+    sorted_idx = np.argsort(counts)[::-1]
+    
+    indexedWords = dict()
+
+    for idx, ss in enumerate(sorted_idx):
+        indexedWords[keys[ss]] = idx+2  
+        #print keys[ss], (idx+2)
+
+    #print np.sum(counts), ' total words ', len(keys), ' unique words'
+
+    return indexedWords
+
+def replaceWordWithIndex(tokenziedArray, indexWords):
+    indexedTweets = []
+
+    for phrase in tokenziedArray: 
+        tweet = []
+        for word in phrase:
+            #print word, str(indexWords[word])
+            t=word.replace(word,str(indexWords[word]))
+            tweet.append(t)
+
+        indexedTweets.append(tweet)
+
+    return indexedTweets
 
 (targets, data)=readCSV('../Resources/Sentiment140/TestingData.csv')
 tokens=tokenzieSentence(data)
-print(tokens)
+freq=wordFrequency(tokens)
+indexWords = indexWords(freq)
+indexTweets = replaceWordWithIndex(tokens, indexWords)
 
-#print tokens
+#print(freq)
+
+
+print indexTweets
