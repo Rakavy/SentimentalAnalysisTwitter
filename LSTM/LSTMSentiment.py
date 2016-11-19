dbdb9282194db8030ff5ca392e7a9e9cf648a1b2
Alexis Grondin <agrondin1@hotmail.com>
2016-11-18 23:35:57 -0500

    small fixes

--- LSTM/LSTMSentiment.py
+++ LSTM/LSTMSentiment.py
@@ -1,7 +1,7 @@
 import pandas as pnd
 import numpy as np
 import nltk
-nltk.download('punkt')
+#nltk.download('punkt')
 
 
 def processString(stro):
@@ -24,16 +24,23 @@ def tokenzieSentence(tweets):
     tokenizedPhrase = []
     for phrase in tweets:
         tokenizedPhrase.append([word[0] for word in nltk.pos_tag(nltk.word_tokenize(phrase)) if word[1] not in ['CC','IN']])
-
-
-
-
     return tokenizedPhrase
 
 
+def getMiniBatches(n, size, shuffle=False):
+    indexLst=np.arange(n, dtype='int32')
+
+    if shuffle:
+        np.random.shuffle(indexLst)
+
+    miniBatches=[indexLst[i*size:(i+1)*size] for i in range(n/size+1)]
+
+    return zip(range(len(miniBatches)), miniBatches)
+
+print(getMiniBatches(40,10))
+print(getMiniBatches(50,6,True))
 
 (targets, data)=readCSV('../Resources/Sentiment140/TestingData.csv')
 tokens=tokenzieSentence(data)
-print(tokens)
 
 #print tokens
