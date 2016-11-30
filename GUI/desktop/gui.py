891ea9ada1161c18662ab8e73b0d5a57643dedea
alex <alex93dussault@gmail.com>
2016-11-30 16:40:11 -0500

    Fixed errors, removes emojis

--- GUI/desktop/gui.py
+++ GUI/desktop/gui.py
@@ -1,20 +1,36 @@
 from tkinter import *
 from twitter import twitterApi 
 
+
 def searchTweet(*args):
-    listTweets = twitterApi(tweet.get())
-    
+    word = tweet.get()
+    word.replace(" ","+")
+    word.replace("#", "%23")
+    listTweets = twitterApi(word)
+    lblTweets.delete('1.0', END) 
+
     for x in listTweets:
+        x = ''.join(c for c in x if c <= '\uFFFF')
         lblTweets.insert(END, x + '\n')
 
-    lblTweets.pack()
-    searchWord.set(tweet.get())
+    searchWord.set(word)
     #foundTweet.set(listTweets)
     
 #def on_click(event):
 
+def convert65536(s):
+    #Converts a string with out-of-range characters in it into a string with codes in it.
+    l=list(s);
+    i=0;
+    while i<len(l):
+        o=ord(l[i]);
+        if o>65535:
+            l[i]="{"+str(o)+"ū}";
+        i+=1;
+    return "".join(l);
+
 root = Tk()
-root.geometry("1000x800+600+250")
+root.geometry("1000x800+800+20")
 mainFrame = Frame(root,background="white")
     
 root.title("Twitter Sentimental Analysis")
@@ -22,6 +38,7 @@ mainFrame.pack(fill=BOTH, expand=True)
 
 mainFrame.columnconfigure(0, weight=1)
 mainFrame.columnconfigure(3, pad=7)
+mainFrame.rowconfigure(5, weight=1)
 
 tweet = StringVar() 
 searchWord = StringVar()
@@ -44,7 +61,7 @@ searchTweets.grid(row=3,column =1, padx=10)
 lstTweets = Label(mainFrame, text="List of tweets:", width=10)
 lstTweets.grid(row=4,column =0,padx=10)
 
-lblTweets = Text(mainFrame,  width=150)
+lblTweets = Text(mainFrame, width=150)
 lblTweets.grid(row=5,column =0, padx=10)
 
 cbtn = Button(mainFrame, text="Close")
